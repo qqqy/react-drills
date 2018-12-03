51b69e0121b1337fc1577f8433e82eb8abded93c
qqqy <frogasaurus@hotmail.com>
2018-12-03 14:30:43 -0700

    app 1 complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      userInput: ''
+    }
+
+    this.processOnChange = this.processOnChange.bind(this)
+  }
+
+  processOnChange = ( e ) => {
+    // console.log(e)
+    let newInputState = e
+    console.log(newInputState)
+    this.setState({
+      userInput: newInputState
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={( e ) => this.processOnChange(e.target.value)} />
+        <p>{this.state.userInput} </p>
       </div>
     );
   }
